23c0e4fedb171335cc9c8fa7fd6df2d3ba47f4d7
Juliette117 <ajuliette7@gmail.com>
2023-12-11 16:03:09 +0100

    feat/exo1

---
+++ TP1/src/main/java/exercices/TP1/Exercice1/Moyenne.java
@@ -0,0 +1,50 @@
+package exercices.TP1.Exercice1;
+
+public class Moyenne {
+
+    public static double getMoyenne(double[] notes) throws IllegalAccessException {
+
+        verifierSi3Notes(notes);
+
+        verifierSiNotesComprisesEntre0et20(notes);
+
+        return calculerMoyenne(notes);
+    }
+
+    /**
+     * Calcule la moyenne en fonction des notes saisies
+     * @param notes double - notes
+     * @return double - la moyenne de l'élève
+     */
+    private static double calculerMoyenne(double[] notes) {
+        double moyenne = 0;
+
+        for (double note: notes) {
+            moyenne += note;
+        }
+
+        return moyenne / notes.length;
+    }
+
+    /**
+     * Vérifie si toutes les notes sont bien comprises entre 0 et 20
+     * @param notes double : les notes
+     */
+    private static void verifierSiNotesComprisesEntre0et20(double[] notes) throws IllegalArgumentException {
+        for (double note: notes) {
+            if (note < 0 || note > 20) {
+                throw new IllegalArgumentException("Les notes doivent être comprises entre 0 et 20 !");
+            }
+        }
+    }
+
+    /**
+     * Lève une exception si le nombre de notes est différent de trois.
+     * @param notes double : Le carnet de notes de l'élève
+     */
+    private static void verifierSi3Notes(double[] notes) throws IllegalArgumentException {
+        if (notes.length != 3) {
+            throw new IllegalArgumentException("Il doit y avoir 3 notes");
+        }
+    }
+}
---
+++ TP1/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import exercices.TP1.Exercice1.Moyenne;
+
+public class Main {
+
+    public static void main(String[] args) throws IllegalAccessException {
+
+     double[] notes = {10, 12, 17};
+        System.out.println(Moyenne.getMoyenne(notes));
+
+    }
+}
+
+
+
